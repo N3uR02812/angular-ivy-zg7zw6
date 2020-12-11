e8078f35c15f12a526b481e1179e506be2ca4fec
N3uR02812 <n3ur0b@gmail.com>
2020-12-11 20:21:56 +0100

    1 file modified

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -1,41 +1,45 @@
-import { Injectable, NgModule, SkipSelf, Optional, InjectionToken } from '@angular/core';
-import { BasicService } from './basicService';
-import { ContainerItemsService } from './containerItemsService';
-import { CategoryService } from './categoryService';
-import { ContainerService } from './containerService';
-import { ODataService } from 'odata-v4-ng';
-import { APP_CONFIG } from '../helper/injectionTokens';
-import { UserService } from './userService';
-import { ShoppingItemsService } from './shoppingItemsService';
-import { GroupService } from './groupService';
+import {
+  Injectable,
+  NgModule,
+  SkipSelf,
+  Optional,
+  InjectionToken
+} from "@angular/core";
+import { BasicService } from "./basicService";
+import { ContainerItemsService } from "./containerItemsService";
+import { CategoryService } from "./categoryService";
+import { ContainerService } from "./containerService";
+import { ODataService } from "odata-v4-ng";
+import { APP_CONFIG } from "../helper/injectionTokens";
+import { UserService } from "./userService";
+import { ShoppingItemsService } from "./shoppingItemsService";
+import { GroupService } from "./groupService";
 // import { EanService } from './eanService';
 
 @NgModule({
-     providers: [
-         ODataService,
-         BasicService,
-         ContainerService,
-         GroupService,
-         ContainerItemsService,
-         ShoppingItemsService,
-         CategoryService,
-         UserService,
-        //  EanService
-     ]
+  providers: [
+    ODataService,
+    BasicService,
+    ContainerService,
+    GroupService,
+    ContainerItemsService,
+    ShoppingItemsService,
+    CategoryService,
+    UserService
+    //  EanService
+  ]
 })
 export class ServiceModule {
-    public static forRoot(config: any) {
-         return {
-              ngModule: ServiceModule,
-              providers: [
-                  {provide: APP_CONFIG, useValue: config}
-              ]
-         };
-    }
+  public static forRoot(config: any) {
+    return {
+      ngModule: ServiceModule,
+      providers: [{ provide: APP_CONFIG, useValue: config }]
+    };
+  }
 
-    public constructor(@Optional() @SkipSelf() parentModule: ServiceModule) {
-         if(parentModule) {
-             throw new Error('ServiceModule has already been imported.');
-         }
+  public constructor(@Optional() @SkipSelf() parentModule: ServiceModule) {
+    if (parentModule) {
+      throw new Error("ServiceModule has already been imported.");
     }
+  }
 }
